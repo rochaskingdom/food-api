5c764f56138604fe4b8e43f47b3557369eb256b6
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-01 18:08:20 -0300

    Implementando content negotiation para retornar JSON ou XML

--- src/main/java/com/vinicius/food/api/controller/CozinhaController.java
+++ src/main/java/com/vinicius/food/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.vinicius.food.api.controller;
 import com.vinicius.food.api.domain.entity.Cozinha;
 import com.vinicius.food.api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas") //, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
-    public List<Cozinha> lisar() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> lisar1() {
+        System.out.println("LISTAR 1");
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Cozinha> lisar2() {
+        System.out.println("LISTAR 2");
         return cozinhaRepository.listar();
     }
 }
