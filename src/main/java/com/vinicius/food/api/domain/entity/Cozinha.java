ee907dd28f43d1ea489b9ef6cb86b759ec7f7176
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-01 02:25:57 -0300

    Conhecendo e usando o Lombok

--- src/main/java/com/vinicius/food/api/domain/entity/Cozinha.java
+++ src/main/java/com/vinicius/food/api/domain/entity/Cozinha.java
@@ -1,19 +1,20 @@
 package com.vinicius.food.api.domain.entity;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
-@EqualsAndHashCode
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String nome;
--- src/main/java/com/vinicius/food/api/domain/entity/Restaurante.java
+++ src/main/java/com/vinicius/food/api/domain/entity/Restaurante.java
@@ -1,5 +1,6 @@
 package com.vinicius.food.api.domain.entity;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,14 +8,14 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@EqualsAndHashCode
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String nome;
