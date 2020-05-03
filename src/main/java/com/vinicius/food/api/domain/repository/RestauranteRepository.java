fbc0a042f7b5621babe2e74765a2ab0c25baade7
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-03 18:10:31 -0300

    Implementando um repositório SDJ customizado

--- src/main/java/com/vinicius/food/api/domain/repository/RestauranteRepository.java
+++ src/main/java/com/vinicius/food/api/domain/repository/RestauranteRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
@@ -23,4 +23,5 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
     List<Restaurante> findTop2ByNomeContaining(String nome);
 
     int countByCozinhaId(Long cozinha);
+
 }
