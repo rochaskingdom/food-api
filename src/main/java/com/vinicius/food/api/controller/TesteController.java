a7f8e6706e17714c0e033caf77372092d0d59bc6
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-03 18:33:19 -0300

    Implementando uma consulta dinâmica com JPQL

--- src/main/java/com/vinicius/food/api/controller/TesteController.java
+++ src/main/java/com/vinicius/food/api/controller/TesteController.java
@@ -59,8 +59,9 @@ public class TesteController {
         return restauranteRepository.findTop2ByNomeContaining(nome);
     }
 
-    @GetMapping("/restaurantes/por-nome-frete")
-    public List<Restaurante> restaurantesPorNomeFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+    @GetMapping("/restaurantes/por-nome-e-frete")
+    public List<Restaurante> restaurantesPorNomeFrete(String nome,
+                                                      BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
         return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
     }
 
