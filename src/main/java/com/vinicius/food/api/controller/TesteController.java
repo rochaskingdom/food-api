d5c13270f779de31296abf0863117375501d18e0
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-03 02:30:26 -0300

    Criando consultas com query methods

--- src/main/java/com/vinicius/food/api/controller/TesteController.java
+++ src/main/java/com/vinicius/food/api/controller/TesteController.java
@@ -1,9 +1,15 @@
 package com.vinicius.food.api.controller;
 
+import com.vinicius.food.api.domain.model.Cozinha;
 import com.vinicius.food.api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -12,8 +18,13 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-//    @GetMapping("/cozinhas/por-nome")
-//    public List<Cozinha> cozinhaNome(@RequestParam("nome") String nome) {
-//        return cozinhaRepository.consultarPorNome(nome);
-//    }
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhaNome(@RequestParam("nome") String nome) {
+        return cozinhaRepository.findTodasByNome(nome);
+    }
+
+    @GetMapping("/cozinhas/unico-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
 }
