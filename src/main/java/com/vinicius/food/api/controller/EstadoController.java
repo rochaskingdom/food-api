6b441903d1e7e1479f55c1cb7e6bbd7893b70ddf
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-02 23:19:06 -0300

    Desafio: implementando serviço REST de estados

--- src/main/java/com/vinicius/food/api/controller/EstadoController.java
+++ src/main/java/com/vinicius/food/api/controller/EstadoController.java
@@ -1,11 +1,15 @@
 package com.vinicius.food.api.controller;
 
+import com.vinicius.food.api.domain.exception.EntidadeEmUsoException;
+import com.vinicius.food.api.domain.exception.EntidadeNaoEncontradaException;
 import com.vinicius.food.api.domain.model.Estado;
 import com.vinicius.food.api.domain.repository.EstadoRepository;
+import com.vinicius.food.api.domain.service.CadastroEstadoService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +20,68 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
+    @Autowired
+    private CadastroEstadoService cadastroEstadoService;
+
     @GetMapping
     public List<Estado> listar() {
         return estadoRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Estado> buscar(@PathVariable Long id) {
+        Estado estado = estadoRepository.buscar(id);
+
+        if (estado != null) {
+            return ResponseEntity.ok(estado);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> adicionar(@RequestBody Estado estado) {
+        try {
+            estado = cadastroEstadoService.adicionar(estado);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(estado);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Estado estado) {
+        try {
+            Estado estadoAtual = estadoRepository.buscar(id);
+
+            if (estadoAtual != null) {
+                BeanUtils.copyProperties(estado, estadoAtual, "id");
+
+                estadoAtual = cadastroEstadoService.adicionar(estadoAtual);
+                return ResponseEntity.ok(estadoAtual);
+            }
+
+            return ResponseEntity.notFound().build();
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Estado> remover(@PathVariable Long id) {
+        try {
+            cadastroEstadoService.remover(id);
+            return ResponseEntity.noContent().build();
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+
+        } catch (EntidadeEmUsoException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
